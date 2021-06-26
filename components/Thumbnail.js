6d290b088203bca77a935cf159f0a9f4799be4fc
Muhammad Nabeel <m.nabeeel96@gmail.com>
2021-06-27 01:10:05 +0500

    'second-commit'

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,7 +4,7 @@ import { forwardRef } from 'react';
 
 const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original/';
-  console.log(result);
+
   return (
     <div
       ref={ref}
